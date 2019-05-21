cd6c123362e4932e7d1147375ccea1f7fa0a180e
PanAeon <vitalii.voloshyn@gmail.com>
2019-05-21 15:31:19 +0100

    remove unused code

--- src/snapshot.ts
+++ src/snapshot.ts
@@ -68,91 +68,7 @@ let grammarPaths : { [key: string]: string } = {}
 
 grammarPaths[program.scope] = program.grammar;
 
-
 const registry = createRegistry(grammarPaths) ; 
-
-// function printSourceLine(testCase: GrammarTestCase, failure: TestFailure) {
-//     const line = testCase.source[failure.srcLine] 
-//     const pos = (failure.line + 1) + ": "
-//     const accents = " ".repeat(failure.start) + "^".repeat(failure.end - failure.start)
-
-//     const termWidth = terminalWidth - pos.length - Padding.length - 5
-
-//     const trimLeft = failure.end > termWidth ? Math.max(0, failure.start - 8) : 0
-
-//     const line1 = line.substr(trimLeft)
-//     const accents1 = accents.substr(trimLeft)
-
-//     console.log(Padding + chalk.gray(pos) + line1.substr(0, termWidth)) 
-//     console.log(Padding +  " ".repeat(pos.length) + accents1.substr(0, termWidth))    
-// }
-
-// function printReason(testCase: GrammarTestCase, failure: TestFailure) {
-//     if (failure.missing && failure.missing.length > 0) {
-//         console.log(chalk.red(Padding + "missing required scopes: ") + chalk.gray(failure.missing.join(" ")))
-//     }
-//     if (failure.unexpected && failure.unexpected.length > 0) {
-//         console.log(chalk.red(Padding + "prohibited scopes: ") + chalk.gray(failure.unexpected.join(" ")))
-//     }
-//     if (failure.actual !== undefined) {
-//         console.log(chalk.red(Padding + "actual: ") + chalk.gray(failure.actual.join(" ")))
-//     }
-// }
-
-// function displayTestResultFull(filename: string, testCase: GrammarTestCase, failures: TestFailure[]): number {
-//     if (failures.length === 0) {
-//         console.log(chalk.green(symbols.ok) + " " + chalk.whiteBright(filename) + ` run successfuly.`)
-//         return TestSuccessful;
-//     } else {
-//         console.log(chalk.red(symbols.err + " " + filename + " failed"))
-//         failures.forEach(failure => {
-//             const {l,s,e} = getCorrectedOffsets(failure)
-//             console.log(Padding + "at [" + chalk.whiteBright(`${filename}:${l}:${s}:${e}`) + "]: ")
-//             printSourceLine(testCase, failure);
-//             printReason(testCase, failure);
-            
-//             console.log("\n")
-//         });
-//         console.log("");
-//         return TestFailed;
-//     }
-    
-// }
-
-// function renderCompactErrorMsg(testCase: GrammarTestCase, failure: TestFailure): string {
-//     let res = ""
-//     if (failure.missing && failure.missing.length > 0) {
-//         res += `Missing required scopes: [ ${failure.missing.join(" ")} ] `
-//     }
-//     if (failure.unexpected && failure.unexpected.length > 0) {
-//         res += `Prohibited scopes: [ ${failure.unexpected.join(" ")} ] `
-//     }
-//     if (failure.actual !== undefined) {
-//         res += `actual scopes: [${failure.actual.join(" ")}]`
-//     }
-//     return res;
-// }
-
-// function displayTestResultCompact(filename: string, testCase: GrammarTestCase, failures: TestFailure[]): number {
-//     if (failures.length === 0) {
-//         console.log(chalk.green(symbols.ok) + " " + chalk.whiteBright(filename) + ` run successfuly.`)
-//         return TestSuccessful;
-//     } else {
-//         failures.forEach(failure => {
-//             console.log(`ERROR ${filename}:${failure.line + 1}:${failure.start + 1}:${failure.end+1} ${renderCompactErrorMsg(testCase, failure)}`)
-            
-//         });
-//         return TestFailed;
-//     }
-// }
-
-// function handleGrammarTestError(filename: string, testCase: GrammarTestCase, reason:any): number {
-//     console.log(chalk.red(symbols.err) + " testcase " + chalk.gray(filename) + " aborted due to an error")
-//     console.log(reason);
-//     return TestFailed;
-// }
-
-// const displayTestResult = program.compact ? displayTestResultCompact : displayTestResultFull;
 
 glob(program.testcases, (err,files0) => {
     if (err !== null) {
@@ -354,18 +270,9 @@ const Added = 2
 
 function printSourceLine(line: String, n: number): number { 
     const pos = (n + 1) + ": "
-    // const accents = " ".repeat(failure.start) + "^".repeat(failure.end - failure.start)
-
-    // const termWidth = terminalWidth - pos.length - Padding.length - 5
-
-    // const trimLeft = failure.end > termWidth ? Math.max(0, failure.start - 8) : 0
-
-    // const line1 = line.substr(trimLeft)
-    // const accents1 = accents.substr(trimLeft)
 
     console.log(Padding + chalk.gray(pos) + line) 
     return pos.length
-    // console.log(Padding +  " ".repeat(pos.length) + accents1.substr(0, termWidth))    
 }
 
 function printAccents(offset: number, from: number, to: number, diff: string) {
