a8f0eed185304e56d8983b1c44c108a0813c3438
Fabian Achammer <fabian.achammer@gmail.com>
2019-09-09 19:20:07 +0200

    used platform-conforming EOL character for split
    This fixes issues on Windows where test cases are wrongly failing

--- src/snapshot/index.ts
+++ src/snapshot/index.ts
@@ -2,6 +2,7 @@
 
 import * as tm from 'vscode-textmate';
 import { AnnotatedLine } from './model';
+import { EOL } from 'os';
 
 
 
@@ -11,7 +12,7 @@ export async function getVSCodeTokens(registry: tm.Registry, scope: string, sour
 
         let ruleStack: tm.StackElement = <any>null;
 
-        return source.split("\n").map((line: string, n: number) => {
+        return source.split(EOL).map((line: string, n: number) => {
             var {tokens, ruleStack:ruleStack1} = grammar.tokenizeLine(line, ruleStack);
             ruleStack = ruleStack1;
 
