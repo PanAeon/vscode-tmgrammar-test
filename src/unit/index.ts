1c3851e7a2654b55cf00fe64875a1076a4a208d7
Nish Tahir <nishtahir@outlook.com>
2020-10-07 11:44:40 -0400

    Allow registry to be created from grammar sources directly

--- src/unit/index.ts
+++ src/unit/index.ts
@@ -7,14 +7,14 @@ import { GrammarTestCase, TestFailure } from './model';
 import { parseGrammarTestCase } from './parsing';
 //import { basename } from 'path';
 
-export {parseGrammarTestCase, GrammarTestCase, TestFailure, missingScopes_}
+export { parseGrammarTestCase, GrammarTestCase, TestFailure, missingScopes_ }
 
 
 export async function runGrammarTestCase(registry: tm.Registry, testCase: GrammarTestCase): Promise<TestFailure[]> {
-    return registry.loadGrammar(testCase.metadata.scope).then((grammar: tm.IGrammar|null) => {
+    return registry.loadGrammar(testCase.metadata.scope).then((grammar: tm.IGrammar | null) => {
 
         if (!grammar) {
-          throw new Error(`Could not load scope ${testCase.metadata.scope}`);
+            throw new Error(`Could not load scope ${testCase.metadata.scope}`);
         }
 
         const assertions = toMap((x) => x.sourceLineNumber, testCase.assertions)
@@ -24,14 +24,14 @@ export async function runGrammarTestCase(registry: tm.Registry, testCase: Gramma
         let failures: TestFailure[] = []
 
         testCase.source.forEach((line: string, n: number) => {
-            var {tokens, ruleStack:ruleStack1} = grammar.tokenizeLine(line, ruleStack);
+            var { tokens, ruleStack: ruleStack1 } = grammar.tokenizeLine(line, ruleStack);
             ruleStack = ruleStack1;
 
             if (assertions[n] !== undefined) {
                 let { testCaseLineNumber, scopeAssertions } = assertions[n];
 
-                scopeAssertions.forEach(({ from, to, scopes: requiredScopes, exclude:excludedScopes }) => {
-                    const xs = tokens.filter((t) =>  from < t.endIndex && to > t.startIndex)
+                scopeAssertions.forEach(({ from, to, scopes: requiredScopes, exclude: excludedScopes }) => {
+                    const xs = tokens.filter((t) => from < t.endIndex && to > t.startIndex)
                     if (xs.length === 0 && requiredScopes.length > 0) {
                         failures.push(<TestFailure>{
                             missing: requiredScopes,
@@ -68,62 +68,71 @@ export async function runGrammarTestCase(registry: tm.Registry, testCase: Gramma
 
 }
 
+export function createRegistryFromGrammars(grammars: Array<{ path: string, content: string }>): tm.Registry {
+    let grammarIndex: { [key: string]: tm.IRawGrammar } = {}
 
-export function createRegistry(grammarPaths: string[]) : tm.Registry {
-  let grammars: { [key: string]: tm.IRawGrammar } = {}
-
-  for (let path of grammarPaths) {
-    let content = fs.readFileSync(path).toString()
-    let rawGrammar = tm.parseRawGrammar(content, path)
-    grammars[rawGrammar.scopeName] = rawGrammar;
-  }
-
-  return new tm.Registry(<tm.RegistryOptions>{
-    loadGrammar: function (scopeName) {
-        if (grammars[scopeName] !== undefined) {
-            return new Promise((c, e) => {
-                c(grammars[scopeName]);
-            });
-        } else {
-            console.warn("grammar not found for '" + scopeName + "'")
-        }
-        return null;
+    for (const grammar of grammars) {
+        const { path, content } = grammar;
+        let rawGrammar = tm.parseRawGrammar(content, path);
+        grammarIndex[rawGrammar.scopeName] = rawGrammar;
     }
-  });
+
+    return new tm.Registry(<tm.RegistryOptions>{
+        loadGrammar: (scopeName) => {
+            if (grammarIndex[scopeName] !== undefined) {
+                return new Promise((fulfill, _) => {
+                    fulfill(grammarIndex[scopeName]);
+                });
+            }
+            console.warn(`grammar not found for "${scopeName}"`)
+            return null;
+        }
+    });
+}
+
+export function createRegistry(grammarPaths: string[]): tm.Registry {
+    return createRegistryFromGrammars(
+        grammarPaths.map((path) => {
+            return {
+                path,
+                content: fs.readFileSync(path).toString()
+            }
+        })
+    );
 }
 
 // ------------------------------------------------------------ helper functions --------------------------------------
 
 
-function missingScopes_(rs:string[], as: string[]):string[] {
-    let i = 0,j = 0;
-    while(i < as.length && j < rs.length) {
-      if (as[i] === rs[j]) {
-          i++;
-          j++;
-      } else {
-          i++
-      }
+function missingScopes_(rs: string[], as: string[]): string[] {
+    let i = 0, j = 0;
+    while (i < as.length && j < rs.length) {
+        if (as[i] === rs[j]) {
+            i++;
+            j++;
+        } else {
+            i++
+        }
     }
 
     return j === rs.length ? [] : rs.slice(j);
 }
 
 
-function toMap<T>(f : (x:T) => number, xs: T[]): { [key: number]: T } {
+function toMap<T>(f: (x: T) => number, xs: T[]): { [key: number]: T } {
     return xs.reduce((m: { [key: number]: T }, x: T) => {
         m[f(x)] = x;
         return m;
     }, {});
 }
 
-function arraysEqual<T>(a: T[], b:T[]) : boolean {
+function arraysEqual<T>(a: T[], b: T[]): boolean {
     if (a === b) { return true };
     if (a === null || b === null) { return false };
     if (a.length !== b.length) { return false };
 
     for (var i = 0; i < a.length; ++i) {
-      if (a[i] !== b[i]) { return false };
+        if (a[i] !== b[i]) { return false };
     }
     return true;
 }
