12891fc5217403773d5b85724eeedf7fed2b8c53
RedCMD <TheRedCMD@gmail.com>
2025-09-12 22:28:27 +1200

    Update VSCode dependencies

--- src/unit/index.ts
+++ src/unit/index.ts
@@ -12,7 +12,7 @@ export async function runGrammarTestCase(registry: tm.Registry, testCase: Gramma
 
     const assertions = toMap((x) => x.sourceLineNumber, testCase.assertions)
 
-    let ruleStack: tm.StackElement = <any>null
+    let ruleStack = tm.INITIAL
 
     let failures: TestFailure[] = []
 
